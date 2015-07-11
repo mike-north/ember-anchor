398e1e2f2a6a37721267e01abe59913800126ad0
Mike North <michael.l.north@gmail.com>
2015-07-11 15:14:32 -0700

    Scroll into position upon initial load

--- addon/mixins/view-support.js
+++ addon/mixins/view-support.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
   didInsertElement() {
     this._super(...arguments);
+    this._scrollToElemPosition();
   },
 
   qpObserver: Ember.observer('controller.anchor', function() {
@@ -10,11 +11,14 @@ export default Ember.Mixin.create({
     if (!elem) {
       return;
     }
-    Ember.run.scheduleOnce('afterRender', () => {
-      let offset = (elem && elem.offset && elem.offset()) ? elem.offset().top : null;
-      if (offset) {
-        Ember.$('body').scrollTop(offset);
-      }
-    });
-  })
+    Ember.run.scheduleOnce('afterRender', this, this._scrollToElemPosition);
+  }),
+
+  _scrollToElemPosition() {
+    let elem = Ember.$(`[data-anchor="${this.get('controller.anchor')}"]`);
+    let offset = (elem && elem.offset && elem.offset()) ? elem.offset().top : null;
+    if (offset) {
+      Ember.$('body').scrollTop(offset);
+    }
+  }
 });
