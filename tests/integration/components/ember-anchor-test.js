adf0a1936147471faef3f94a7ba140b50c39351a
Mike North <michael.l.north@gmail.com>
2018-11-09 13:19:25 -0800

    chore: no more jquery in tests

--- tests/integration/components/ember-anchor-test.js
+++ tests/integration/components/ember-anchor-test.js
@@ -12,12 +12,7 @@ module('Integration | Component | ember anchor', function(hooks) {
 
     await render(hbs`{{ember-anchor}}`);
 
-    assert.equal(
-      this.$()
-        .text()
-        .trim(),
-      ''
-    );
+    assert.equal(this.element.innerText.trim(), '');
 
     // Template block usage:
     await render(hbs`
@@ -26,11 +21,6 @@ module('Integration | Component | ember anchor', function(hooks) {
       {{/ember-anchor}}
     `);
 
-    assert.equal(
-      this.$()
-        .text()
-        .trim(),
-      'template block text'
-    );
+    assert.equal(this.element.innerText.trim(), 'template block text');
   });
 });
